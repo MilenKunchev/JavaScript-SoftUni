8fbe4873eff15feba912f7879f069c6676d8cf7c
MilenKunchev <milen_kunchev@abv.bg>
2018-04-26 14:46:13 +0300

    Introduction to JS - upload

---
+++ Introduction to JavaScript/01_Sum3Numbers.js
@@ -0,0 +1,11 @@
+function  sumNumbers(a,b,c) {
+//     let a = Number(input[0]);
+//     let b = Number(input[1]);
+//     let c = Number(input[2]);
+
+    let sum = a+b+c;
+    console.log(sum);
+}
+
+sumNumbers(1,2,3);
+// sumNumbers(['4','4','4']);
