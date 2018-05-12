29ba16f843037e99f23f05858239e2373f60d42b
Milen Kunchev <32412558+MilenKunchev@users.noreply.github.com>
2018-05-12 18:10:14 +0300

    Update 22_Monthly Calendar.js

--- Expressions, Conditional Statements and Loops/22_Monthly Calendar.js
+++ Expressions, Conditional Statements and Loops/22_Monthly Calendar.js
@@ -1,9 +1,69 @@
-function calendar(date) {
+function calendar(input) {
 
-    let [day, month, year] = date;
+    let [currentDay, month, year] = input;
 
-    console.log(year);
+
+    let date = new Date(year, month - 1, currentDay);
+    let currentMonthDays = new Date(year, month, 0).getDate();
+    let prevMonthDays = new Date(year, month - 1, 0).getDate();
+    let daysFromPrevMonth = new Date(year, month - 1, 1).getDay() - 1;
+    let daysFronNextMonth = 6 - new Date(year, month - 1, currentMonthDays).getDay();
+
+    let html = `<table>\n`;
+
+     html += `<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>\n`;
+
+    let weekDaysCounter = 0;
+
+    //Prev month
+    html += `<tr>`;
+
+    for (let day = prevMonthDays - daysFromPrevMonth; day <= prevMonthDays; day++) {
+
+        html += `<td class="prev-month">${day}</td>`;
+        weekDaysCounter++;
+
+        if (weekDaysCounter === 7) {
+            html += `</tr>\n`;
+            html += `<tr>`;
+
+            weekDaysCounter = 0;
+        }
+    }
+    // Currenth month
+    for (let day = 1; day <= currentMonthDays; day++) {
+
+        if (day === currentDay) {
+
+            html += `<td class="today">${day}</td>`;
+            weekDaysCounter++;
+
+        } else {
+            html += `<td>${day}</td>`;
+            weekDaysCounter++;
+        }
+
+        if (weekDaysCounter === 7) {
+            html += `</tr>\n`;
+            if (day < currentMonthDays) {
+                html += `<tr>`;
+            }
+
+            weekDaysCounter = 0;
+        }
+    }
+
+    // Next month
+    for (let day = 1; day <= daysFronNextMonth; day++) {
+        html += `<td class="next-month">${day}</td>`;
+    }
+
+    if (daysFronNextMonth !== 0) {
+        html += `</tr>\n`;
+    }
+
+    html += `</table>`;
+    //console.log(html);
+    return html;
 
 }
-
-calendar([24, 12, 2012]);
